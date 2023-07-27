19c2b53af681333feb2dfd39f1b903cfd3132163
satya <satyabrata.b@techouts.co.in>
2023-07-27 13:00:06 +0530

    implemented latest

--- src/componetes/bluetoothConnection/index.js
+++ src/componetes/bluetoothConnection/index.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Button, PermissionsAndroid, Platform, Text, View } from 'react-native';
-import BleManager from 'react-native-ble-plx';
+import { FlatList, PermissionsAndroid, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+
+const _BleManager = new BleManager();
 
 const BluetoothComponent = () => {
   const [scanning, setScanning] = useState(false);
   const [devices, setDevices] = useState([]);
-
+  const [connectedDevice, setConnectedDevice] = useState({});
+  const [characteristics, setCharacteristics] = useState([]);
+  const [isBluetoothInitialized,setIsBluetoothInitialized]=useState(false)
   useEffect(() => {
     const initializeBluetooth = async () => {
       try {
@@ -15,13 +19,7 @@ const BluetoothComponent = () => {
           return;
         }
 
-        BleManager.start({ showAlert: false })
-          .then(() => {
-            console.log('BleManager initialized successfully');
-          })
-          .catch((error) => {
-            console.error('Error initializing BleManager:', error);
-          });
+        setIsBluetoothInitialized(true);
       } catch (error) {
         console.error('Error initializing Bluetooth:', error);
       }
@@ -30,22 +28,23 @@ const BluetoothComponent = () => {
     initializeBluetooth();
 
     return () => {
-      BleManager.stopScan();
+      _BleManager.stopDeviceScan();
     };
   }, []);
 
   const startScan = () => {
-    setScanning(true);
-    setDevices([]);
-
-    BleManager.startDeviceScan([], null, (error, results) => {
+    _BleManager.startDeviceScan(null, { allowDuplicates: false }, async (error, device) => {
       if (error) {
         console.error('Error scanning for devices:', error);
-      } else {
-        console.log('Scanning results:', results);
-        setDevices(results);
-        setScanning(false);
-      }})
+        _BleManager.stopDeviceScan();
+        return;
+      }
+
+      console.log(device.localName, device.name);
+      if (device.localName === 'Test' || device.name === 'Test') {
+        setDevices(prevDevices => [...prevDevices, device]);
+      }
+    });
   };
 
   const requestBluetoothPermission = async () => {
@@ -56,27 +55,91 @@ const BluetoothComponent = () => {
     return true;
   };
 
-  const connectToDevice = async (device) => {
-    try {
-      await device.connect();
-      console.log('Connected to device:', device.name);
-      // You can now perform any actions with the connected device.
-    } catch (error) {
-      console.error('Error connecting to device:', error);
-    }
+  const connectDevice = (device) => {
+    _BleManager.stopDeviceScan();
+    _BleManager.connectToDevice(device.id)
+      .then(async (connectedDevice) => {
+        await connectedDevice.discoverAllServicesAndCharacteristics();
+        setConnectedDevice(connectedDevice);
+        setDevices([]);
+        connectedDevice.services().then((service) => {
+          for (const ser of service) {
+            ser.characteristics().then((characteristic) => {
+              setCharacteristics((prevCharacteristics) => [...prevCharacteristics, characteristic]);
+            });
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('Error connecting to device:', error);
+      });
+  };
+
+  const disconnectDevice = () => {
+    connectedDevice.cancelConnection();
+    setConnectedDevice({});
   };
 
   return (
-    <View>
-      <Button title="Start Scan" onPress={startScan} disabled={scanning} />
-      {devices.map((device) => (
-        <View key={device.id}>
-          <Text>{device.name}</Text>
-          <Button title="Connect" onPress={() => connectToDevice(device)} />
+    <View style={styles.mainContainer}>
+      {devices.length === 0 ? (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <TouchableOpacity activeOpacity={0.6} onPress={()=>startScan()} style={styles.circleView}>
+            <Text style={styles.boldTextStyle}>Tap to Start Scanning</Text>
+          </TouchableOpacity>
         </View>
-      ))}
+      ) : Object.keys(connectedDevice).length !== 0 ? (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ marginBottom: 12, textAlign: 'center' }}>Tap button to disconnect device.</Text>
+          <TouchableOpacity activeOpacity={0.6} onPress={disconnectDevice} style={styles.circleView}>
+            <Text style={styles.boldTextStyle}>Tap to Disconnect</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <FlatList
+          style={{ flex: 1 }}
+          data={devices}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={(items) => (
+            <TouchableOpacity
+              activeOpacity={0.6}
+              onPress={() => connectDevice(items.item)}
+              style={{
+                width: '100%',
+                paddingVertical: 10,
+                paddingHorizontal: 12,
+                borderWidth: 1,
+                borderRadius: 10,
+              }}>
+              <Text style={{ color: 'black', fontSize: 18 }}>{items.item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  circleView: {
+    width: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    height: 250,
+    borderRadius: 150,
+    borderWidth: 1,
+  },
+  boldTextStyle: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default BluetoothComponent;
